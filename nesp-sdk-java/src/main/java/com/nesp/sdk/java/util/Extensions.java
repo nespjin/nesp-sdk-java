b0a0da36f32038077c4fdd4d22ecc2a7d2792d94
靳兆鲁 <1756404649@qq.com>
2021-11-03 22:04:08 +0800

    Add Extensions

---
+++ nesp-sdk-java/src/main/java/com/nesp/sdk/java/util/Extensions.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 The NESP Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nesp.sdk.java.util;
+
+/**
+ * Team: NESP Technology
+ * Author: <a href="mailto:1756404649@qq.com">JinZhaolu Email:1756404649@qq.com</a>
+ * Time: Created 2021/11/3 21:58
+ * Description:
+ **/
+public abstract class Extensions<T> {
+
+    private final T value;
+
+    public Extensions(final T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
