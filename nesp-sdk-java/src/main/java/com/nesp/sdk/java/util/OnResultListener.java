b75dfa36c166226befa5485510a95539d8ea174e
靳兆鲁 <1756404649@qq.com>
2021-11-03 22:04:08 +0800

    Change button type Add tag selection page Add Extensions

---
+++ nesp-sdk-java/src/main/java/com/nesp/sdk/java/util/OnResultListener.java
@@ -0,0 +1,11 @@
+package com.nesp.sdk.java.util;
+
+/**
+ * Team: NESP Technology
+ * Author: <a href="mailto:1756404649@qq.com">JinZhaolu Email:1756404649@qq.com</a>
+ * Time: Created 2021/11/7 18:31
+ * Description:
+ **/
+public interface OnResultListener<T> {
+    void onResult(T result);
+}
