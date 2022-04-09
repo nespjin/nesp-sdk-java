26acef3b7ccc70c76c060f0d2c655851c1f3d15c
靳兆鲁 <1756404649@qq.com>
2022-04-10 00:15:14 +0800

    [Update] Cache2

--- nesp-sdk-java/src/main/java/com/nesp/sdk/java/cache/Cache2.java
+++ nesp-sdk-java/src/main/java/com/nesp/sdk/java/cache/Cache2.java
@@ -15,6 +15,7 @@
 
 package com.nesp.sdk.java.cache;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -39,49 +40,54 @@ public abstract class Cache2 implements Cache {
         return getByte(key, (byte) 0);
     }
 
-    abstract Byte getByte(String key, byte defValue);
+    public abstract Byte getByte(String key, byte defValue);
 
     @Override
     public Short getShort(final String key) {
         return getShort(key, (short) 0);
     }
 
-    abstract Short getShort(String key, short defValue);
+    public abstract Short getShort(String key, short defValue);
 
     @Override
     public Integer getInt(final String key) {
         return getInt(key, 0);
     }
 
-    abstract Integer getInt(String key, int defValue);
+    public abstract Integer getInt(String key, int defValue);
 
     @Override
     public Long getLong(final String key) {
         return getLong(key, 0L);
     }
 
-    abstract Long getLong(String key, long defValue);
+    public abstract Long getLong(String key, long defValue);
 
     @Override
     public Float getFloat(final String key) {
         return getFloat(key, 0f);
     }
 
-    abstract Float getFloat(String key, float defValue);
+    public abstract Float getFloat(String key, float defValue);
 
     @Override
     public Double getDouble(final String key) {
         return getDouble(key, 0d);
     }
 
-    abstract Double getDouble(String key, double defValue);
+    public abstract Double getDouble(String key, double defValue);
 
     @Override
     public String getString(final String key) {
         return getString(key, "");
     }
 
-    abstract String getString(String key, String defValue);
+    public abstract String getString(String key, String defValue);
 
-    abstract Set<String> getStringSet(String key, Set<String> defValue);
+    @Override
+    public Set<String> getStringSet(final String key) {
+        return getStringSet(key, new HashSet<>());
+    }
+
+    public abstract Set<String> getStringSet(String key, Set<String> defValue);
 }
