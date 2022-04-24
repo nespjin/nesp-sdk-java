b07f5764b41d20bbda92419ceb4ad431f70e1e29
靳兆鲁 <1756404649@qq.com>
2022-04-25 00:52:12 +0800

    Add $ class

---
+++ nesp-sdk-java/src/main/java/com/nesp/sdk/java/lang/$.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022.   NESP Technology.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package com.nesp.sdk.java.lang;
+
+/**
+ * Team: NESP Technology
+ *
+ * @author <a href="mailto:1756404649@qq.com">JinZhaolu Email:1756404649@qq.com</a>
+ * Time: Created 4/25/2022 12:50 AM
+ * Description:
+ **/
+public class $ {
+}
